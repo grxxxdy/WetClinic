704c08eda45a59963386c1710acbfef374b84b9a
grxxxdy <mozjukhin@gmail.com>
2024-10-14 12:06:58 +0300

    Initial commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector(".menu-toggle");
+const header = document.querySelector(".menu");
+menuToggle.addEventListener("click", ()=>{
+    header.classList.toggle("menu-active");
+});
+
+//# sourceMappingURL=index.44983732.js.map
